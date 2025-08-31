92c76d821b24ae1e55cf1cc11988f9736985b238
hissari-bacha <ghourimuzammil49@gmail.com>
2025-08-31 23:11:19 +0500

    Update config.js

--- config.js
+++ config.js
@@ -35,7 +35,7 @@ BOT_NAME: process.env.BOT_NAME || "꧁HISSARI-BACHA-MD꧂",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "꧁HISSARI-BACHA-MD꧂",
 // type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
@@ -53,7 +53,7 @@ LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar ⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
